0cfd09436acdc71d3e3248ae1dd2dd089e6f4a34
Sarah Smith <155038730+sarahlibx@users.noreply.github.com>
2025-01-16 19:11:36 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const newBackgroundFooter = document.querySelector(".contact");
+const newTagline = document.querySelector(".tagline");
+const addressUpdate = document.querySelector("address");
+const emphasizeWords = document.querySelector("h3");
+
+newBackgroundFooter.style.backgroundColor = "#d7ddf3";
+newTagline.innerText = "Say it with roses.";
+addressUpdate.style.lineHeight = "1.5em";
+addressUpdate.style.fontSize = "1.75em";
+emphasizeWords.innerHTML = "We're here for you <em>every day</em> of the week";
